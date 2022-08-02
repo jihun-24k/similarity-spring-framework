a4d5cd92a9feb1b2488a861cf1cfd6447788c659
jihun-24k <gi020477@gmail.com>
2022-08-02 21:48:40 +0900

    Refactor: Container가 자동으로 모든 컴포넌트를 스캔하여 검색, 검색된 것들을 객체화, 저장

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -16,8 +16,10 @@ public class Container {
     static {
         objects = new HashMap<>();
 
-        objects.put(ArticleController.class, new ArticleController());
-        objects.put(HomeController.class, new HomeController());
+        Reflections ref = new Reflections("com.ll.exam");
+        for(Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)){
+            objects.put(cls, Util.cls.newObj(cls,null));
+        }
     }
 
     public static <T> T getObj(Class cls){
