e12eef3edede5c9297b2e2a5d55a10df7eca18b9
jihun-24k <gi020477@gmail.com>
2022-08-02 11:13:32 +0900

    Chore: reflections 라이브러리 추가

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -2,6 +2,9 @@ package com.ll.exam;
 
 import com.ll.exam.article.controller.ArticleController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Container {
     private static final ArticleController articleController;
 
@@ -11,5 +14,9 @@ public class Container {
 
     public static ArticleController getArticleController() {
         return articleController;
+    }
+
+    public static List<String> getAllControllerNames() {
+        return new ArrayList<>();
     }
 }
---
+++ src/main/java/com/ll/exam/home/article/HomeController.java
@@ -0,0 +1,7 @@
+package com.ll.exam.home.article;
+
+import com.ll.exam.annotation.Controller;
+
+@Controller
+public class HomeController {
+}
