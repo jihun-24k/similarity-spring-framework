21b30265d942cb7c16138b14a654d48d4d5408a4
jihun-24k <gi020477@gmail.com>
2022-08-03 15:06:53 +0900

    Feat: 메인 페이지 생성, 기본 url을 메인으로 설정

--- src/main/java/com/ll/exam/home/article/HomeController.java
+++ src/main/java/com/ll/exam/home/article/HomeController.java
@@ -1,7 +1,13 @@
 package com.ll.exam.home.article;
 
+import com.ll.exam.Rq;
 import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("/usr/home/main")
+    public void showMain(Rq rq) {
+        rq.println("메인 페이지");
+    }
 }
